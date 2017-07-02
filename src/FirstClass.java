1c848c91bdde01eea4e30e9229b60960847c55a8
Richard Widtmann <richardwidtmann@Richards-MBP.attlocal.net>
2017-07-02 15:35:34 -0500

    First commit

---
+++ src/FirstClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created by richardwidtmann on 7/1/17.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+
+        System.out.println("Testing first Java class");
+    }
+}
